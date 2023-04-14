9faee9dd79546b521d184a1622c4ea7558905d8d
huyhaq <119866766+huyhaq@users.noreply.github.com>
2023-04-14 20:41:43 +0700

    1 file modified

--- script.js
+++ script.js
@@ -3,13 +3,13 @@ let bankName = prompt('Nhap ten ngan hang:');
 let bankPercent = prompt('Nhap lai suat:');
 
 let year = prompt('Nhap so nam:');
-let money = prompt('Nhap so tien:');;
+let money = prompt('Nhap so tien:');
 
 let moneyInOneYear = money * (bankPercent / ONE_HUNDRED_PERCENT);
 
 let interestMoney = year * moneyInOneYear;
 
-let total = money + interestMoney;
+let total = +money + +(interestMoney);
 
 let message = `
 <h1>Ung Dung Tinh Lai suat cua ngan hang: ${bankName}</h1>
